ecf92dcd9c5caf40aec928f91488f34a4321bc29
RenatNig <trackid788@gmail.com>
2023-03-05 02:52:47 +0500

    added project

---
+++ GameStore.Domain/Abstract/IGameRepository.cs
@@ -0,0 +1,10 @@
+using System.Collections.Generic;
+using GameStore.Domain.Entities;
+
+namespace GameStore.Domain.Abstract
+{
+    public interface IGameRepository
+    {
+        IEnumerable<Game> Games { get; }
+    }
+}
---
+++ GameStore.Domain/Entities/Game.cs
@@ -0,0 +1,13 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace GameStore.Domain.Entities
+{
+    public class Game
+    {
+        public int GameId { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Category { get; set; }
+        public decimal Price { get; set; }
+    }
+}
