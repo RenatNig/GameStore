7071bca0a6c384027c3bf4b76e6a11ace43a28bb
RenatNig <trackid788@gmail.com>
2023-03-08 02:54:53 +0500

    Revert "Revert "модуль тесты для новых методов""
    This reverts commit d97f0b276bda37c93b98c19f94728f502fe093b0.

--- GameStore.UnitTests/UnitTest2.cs
+++ GameStore.UnitTests/UnitTest2.cs
@@ -19,29 +19,26 @@ namespace GameStore.UnitTests
         {
             //организация
             HtmlHelper htmlHelper = null;
-            
             PagingInfo pagingInfo = new PagingInfo()
             {
                 CurrentPage = 2,
                 ItemsPerPage = 4,
                 TotalItems = 10
             };
-
             string someFunc(int i)
             {
                 return "Page" + i;
             }
-            
             Func<int, string> pageUrlDelegate = new Func<int, string>(someFunc);
             
             //действие
-            var result = PagingHelpers.PageLinks(htmlHelper, pagingInfo, pageUrlDelegate);
+            var result = htmlHelper.PageLinks(pagingInfo, pageUrlDelegate);
+            
             //утверждение
             Assert.AreEqual(@"<a class=""btn btn-default"" href=""Page1"">1</a>"
                             + @"<a class=""btn btn-default btn-primary selected"" href=""Page2"">2</a>"
                             + @"<a class=""btn btn-default"" href=""Page3"">3</a>",
                 result.ToString());
-
         }
     }
 }
